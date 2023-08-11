5e01dd4a8c55b15457bb4f9e0e92311ffa7dad2c
Daniil <68585455+mrwhiteered@users.noreply.github.com>
2023-08-11 18:43:40 +0400

    3
    3

--- js/slowContent.js
+++ js/slowContent.js
@@ -1,14 +1,26 @@
-const fadeInSection = document.querySelector('.fade-in-section');
-const fadeInSectionContent = document.querySelector('.fade-in-content');
+function resetStyles(elem) {
+    elem.style.opacity = '0';
+    elem.style.transform = 'translateY(200px)';
+}
 
 function fadeInOnScroll() {
-    const distanceFromTop = fadeInSection.getBoundingClientRect().top;
-    const windowHeight = window.innerHeight;
+    const fadeInSectionList = document.querySelectorAll('.fade-in');
 
-    if (distanceFromTop - windowHeight <= 0) {
-        fadeInSectionContent.style.opacity = '1';
-        window.removeEventListener('scroll', fadeInOnScroll);
-    }
+    fadeInSectionList.forEach((fadeInSection) => {
+        // const fadeInSectionContent = fadeInSection.querySelector('.fade-in-content');
+        const distanceFromTop = fadeInSection.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+        let speedShow = parseFloat(fadeInSection.getAttribute('data-speed'));
+            
+        if (distanceFromTop - windowHeight + 200 <= 0) {
+            fadeInSection.style.opacity = '1';
+            fadeInSection.style.transform = 'translateY(0)';
+            fadeInSection.style.transition = `opacity ${speedShow}s ease-in-out, transform ${speedShow}s ease-in-out`;
+        }
+         else { 
+            resetStyles(fadeInSection);
+         }
+    });
 }
 
 window.addEventListener('scroll', fadeInOnScroll);
