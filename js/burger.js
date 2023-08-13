a75a3ccd6c25aaacae5a619fed84942f95a8924c
Daniil <68585455+mrwhiteered@users.noreply.github.com>
2023-08-13 11:45:04 +0400

    v1/3 adaptiv

--- js/burger.js
+++ js/burger.js
@@ -1,7 +1,31 @@
 const btn = document.querySelector('.burger');
 const menu = document.querySelector('.burger_menu');
 
-btn.addEventListener('click', function(){
-	menu.classList.toggle('burger_menu_on');
-	btn.classList.toggle('active');
-})
+function closeMenu() {
+    menu.classList.remove('burger_menu_on');
+    btn.classList.remove('active');
+}
+
+btn.addEventListener('click', function(event) {
+    event.stopPropagation();
+    menu.classList.toggle('burger_menu_on');
+    btn.classList.toggle('active');
+});
+
+// Закрывать меню при клике вне области меню
+document.addEventListener('click', function(event) {
+    const target = event.target;
+    if (!menu.contains(target) && !btn.contains(target)) {
+        closeMenu();
+    }
+});
+
+// Остановка всплытия события при клике внутри меню
+menu.addEventListener('click', function(event) {
+    event.stopPropagation();
+});
+
+// Остановка всплытия события при клике на кнопке
+btn.addEventListener('click', function(event) {
+    event.stopPropagation();
+});
