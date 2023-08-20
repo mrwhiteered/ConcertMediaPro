2ace1e111c8100a4c68b8b28fc9100fe7b54ab13
Daniil <68585455+mrwhiteered@users.noreply.github.com>
2023-08-20 14:28:57 +0400

    video pleer

---
+++ js/videoPlay.js
@@ -0,0 +1,96 @@
+let listWorks = document.getElementById('list__works');
+let mainVideo = document.getElementById('videoPlayer');
+
+let playList = ['intro.mov', 'myVideo1.mp4', 'intro.mov', 'intro.mov'];
+let playListLength = playList.length;
+let previousLiElement = null; // Для хранения предыдущего элемента <li>
+
+let videoWrap = document.getElementById('video__wrap'); // Получаем контейнер для видео
+
+let preloadVideo = new Audio(); // Создаем новый элемент Audio (который также загружает видео)
+preloadVideo.autoplay = false; // Отключаем автовоспроизведение
+
+function preloadNextVideo(videoIndex) {
+    let nextVideoIndex = (videoIndex + 1) % playList.length; // Индекс следующего видео
+    let nextVideoPath = `video/works/${playList[nextVideoIndex]}`;
+    
+    preloadVideo.src = nextVideoPath; // Загружаем следующее видео
+}
+
+let number =0;
+
+for (let i = 0; i < playListLength; i++) {
+
+
+
+    let liElem = document.createElement('li');
+    liElem.setAttribute('id', 'video_' + i);
+    liElem.setAttribute('class', 'works__video_item');
+
+    let videoElement = document.createElement('video');
+    videoElement.setAttribute("id", 'video' + i);
+    videoElement.setAttribute("controls", "");
+    videoElement.setAttribute("muted", "");
+    videoElement.setAttribute("loop", "");
+
+    let sourceElement = document.createElement("source");
+    let sourcePath = "../video/works/" + playList[i];
+    sourceElement.src = sourcePath;
+    sourceElement.setAttribute("type", "video/mp4");
+
+    videoElement.appendChild(sourceElement);
+    liElem.appendChild(videoElement);
+
+    if (i === 0) {
+        liElem.classList.add('activeVideo');
+        listWorks.appendChild(liElem);
+        previousLiElement = liElem; // Сохраняем первый элемент <li>
+    } else {
+        // Вставляем остальные элементы после предыдущего элемента <li>
+        listWorks.insertBefore(liElem, previousLiElement.nextSibling);
+        previousLiElement = liElem; // Обновляем предыдущий элемент <li>
+    }
+
+
+}
+
+function nextVideo() {
+    if (number==playList.length - 1){
+        number=0;
+    } else {
+        number++;
+    }
+    preloadNextVideo(number);
+
+     let liVideo = document.querySelectorAll('.works__video_item');
+
+
+   for (let j =0; liVideo.length > j;j++){
+    liVideo[j].classList.remove('activeVideo')
+   }
+    liVideo[number].classList.add('activeVideo');
+    
+    mainVideo.src=`../video/works/${playList[number]}`;
+}
+
+function previousVideo(){
+	if (number==0){
+		number=playList.length -1;
+	} else{
+		number--;
+	}
+    preloadNextVideo(number);
+    mainVideo.src=`../video/works/${playList[number]}`;
+    let liVideo = document.querySelectorAll('.works__video_item');
+
+
+    for (let j =0; liVideo.length > j;j++){
+     liVideo[j].classList.remove('activeVideo')
+    }
+     liVideo[number].classList.add('activeVideo');
+     
+     mainVideo.src=`../video/works/${playList[number]}`;
+ }
+
+
+
