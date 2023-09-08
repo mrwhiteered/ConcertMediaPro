a6f064a20cdb15f83b7ae75400eb7efd54f95a17
Daniil <belyaevred@mail.ru>
2023-09-08 17:06:31 +0300

    t2
    t2

---
+++ js/info_show.js
@@ -0,0 +1,40 @@
+// Находим все элементы .s2__item
+let items = document.querySelectorAll('.s2__item');
+let flag = false;
+
+// Добавляем обработчик события click к каждому элементу .s2__item
+items.forEach(function (item) {
+    item.addEventListener('click', function () {
+        
+        if (!flag){
+            // Находим все элементы <li> с классом "s2__include_item" внутри текущего элемента .s2__item
+            let includeItems = item.querySelectorAll('.s2__include_item');
+            
+            includeItems.forEach(function (item, i) {
+                // Переключаем класс для добавления анимации
+                item.classList.add('s2_active');
+
+                // Устанавливаем продолжительность анимации в зависимости от значения i
+                var animationDuration = (i + 1) * 0.3 + 's'; // Например, 0.2s, 0.4s, 0.6s и так далее
+                item.style.transition = 'all ' + animationDuration;
+            });
+            
+            flag = true;
+
+        } else {
+            // Находим все элементы <li> с классом "s2__include_item" внутри текущего элемента .s2__item
+            let includeItems = item.querySelectorAll('.s2__include_item');
+            
+            includeItems.forEach(function (item, i) {
+                // Переключаем класс для удаления анимации
+                item.classList.remove('s2_active');
+
+                // Устанавливаем продолжительность анимации в зависимости от значения i
+                var animationDuration = 1.2 - (i * 0.3) + 's'; // Например, 0.2s, 0.4s, 0.6s и так далее
+                item.style.transition = 'all ' + animationDuration;
+            });
+            
+            flag = false;
+        }
+    });
+});
